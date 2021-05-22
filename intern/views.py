c374fa3675f7eb3d79d62105990ded5877572fbc
deepak_manwal <deepak.manwal@gmail.com>
2021-05-22 13:23:09 +0530

    Resolved conflicts

--- intern/views.py
+++ intern/views.py
@@ -2,14 +2,22 @@ import intern
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Country
-from .models import State
-from .models import City
+from .models import Country, Post, State, City
 
 # Create your views here.
 def home(request):
     countries = Country.objects.all()
-    print("this is the data we get",request.GET)
+    if request.method=="POST":
+        print("all data is hear" ,request.POST)
+        name=request.POST['Your_name']
+        birthdate=request.POST['birthDate']
+        country=request.POST['country']
+        state=request.POST['state']
+        city=request.POST['city']
+        ins=Post(name=name,birthDate=birthdate,country=country,state=state,city=city)
+        ins.save()
+        
+    # print("this is the data we get",request.POST)
     return render(request,'intern/home.html', {'countries': countries})
 
 #  to populate state dropdown when country is selected.
