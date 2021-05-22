704021b95f254de2e69fa16cff4908d750b838b9
deepak_manwal <deepak.manwal@gmail.com>
2021-05-22 11:22:10 +0530

    Model added

--- web/intern/models.py
+++ web/intern/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    name=models.CharField
+    name=models.CharField(max_length=1000)
+    birthDate=models.CharField(max_length=1000)
+    country=models.CharField(max_length=1000)
+    state=models.CharField(max_length=1000)
+    city=models.CharField(max_length=1000)
+
+
