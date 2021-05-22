de125a5629e746f040464a1d4ebbf9449747c556
deepak_manwal <deepak.manwal@gmail.com>
2021-05-22 18:47:18 +0530

    Validation added

--- intern/urls.py
+++ intern/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home,name='intern-home'),
-    path('country/<int:id>', views.state_view,name='intern-state'),
-    path('state/<int:id>', views.city_view,name='intern-city'),
+    path('country/<name>', views.state_view,name='intern-state'),
+    path('state/<name>', views.city_view,name='intern-city'),
     path('persons/',views.persons_view,name='intern-persons'),
     path('about/',views.about,name='intern-about')
 ]
--- intern/views.py
+++ intern/views.py
@@ -7,27 +7,34 @@ from .models import Country, Post, State, City
 # Create your views here.
 def home(request):
     countries = Country.objects.all()
+    success = 0
+    error = 0
     if request.method=="POST":
-        print("all data is hear" ,request.POST)
-        name=request.POST['Your_name']
-        birthdate=request.POST['birthDate']
-        country=request.POST['country']
-        state=request.POST['state']
-        city=request.POST['city']
-        ins=Post(name=name,birthDate=birthdate,country=country,state=state,city=city)
-        ins.save()
+        name=request.POST['Your_name'].strip()
+        birthdate=request.POST['birthDate'].strip()
+        country=request.POST['country'].strip()
+        state=request.POST['state'].strip()
+        city=request.POST['city'].strip()
+        if(name == '' or birthdate == '' or country == '' or state == '' or city == '' ):
+            error = 1
+        else:
+            ins=Post(name=name,birthDate=birthdate,country=country,state=state,city=city)
+            ins.save()
+            success = 1
 
     # print("this is the data we get",request.POST)
-    return render(request,'intern/home.html', {'countries': countries})
+    return render(request,'intern/home.html', {'countries': countries, 'success': success, 'error': error})
 
 #  to populate state dropdown when country is selected.
-def state_view(request, id):
-    states = State.objects.filter(country_id=id)
+def state_view(request, name):
+    country = Country.objects.get(name=name)
+    states = State.objects.filter(country_id=country.id)
     return render(request,'intern/state.html', {'states': states})
 
 #  to populate city dropdown when state is selected.
-def city_view(request,id):
-    cities = City.objects.filter(state_id=id)
+def city_view(request, name):
+    state = State.objects.get(name=name)
+    cities = City.objects.filter(state_id=state.id)
     return render(request, 'intern/city.html', {'cities': cities})
 
 # List of persons
