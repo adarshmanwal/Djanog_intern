33cd2e5c77df5e2b46e95b671f60be9f27366da7
Deepak Manwal <manwal@Deepaks-MacBook-Pro.local>
2021-05-22 12:21:04 +0530

    State model and data migration added

--- intern/models.py
+++ intern/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Post(models.Model):
     name=models.CharField(max_length=1000)
     birthDate=models.CharField(max_length=1000)
@@ -8,11 +7,13 @@ class Post(models.Model):
     state=models.CharField(max_length=1000)
     city=models.CharField(max_length=1000)
 
-# Create your models here.
 class Country(models.Model):
     id=models.IntegerField(primary_key=True)
     sortname=models.CharField(max_length=5)
     name=models.CharField(max_length=1000)
     phonecode=models.CharField(max_length=20)
 
-
+class State(models.Model):
+    id=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=1000)
+    country_id=models.IntegerField()
--- intern/views.py
+++ intern/views.py
@@ -6,7 +6,6 @@ from .models import Country
 # Create your views here.
 def home(request):
     countries = Country.objects.all()
-    print(countries)
     return render(request,'intern/home.html', {'countries': countries})
 
 def about(request):
